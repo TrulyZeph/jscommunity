a1abb65bb57a47110e5b5da78b3d7e465277bded
Zeph <agwproxies@gmail.com>
2025-06-30 15:58:53 -0400

    Delete netlify/functions/get-profiles.js

--- netlify/functions/get-profiles.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-
-exports.handler = async function(event, context) {
-  const { data, error } = await supabase.from('profiles').select('*').order('id', { ascending: true });
-  if (error) return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
-  return { statusCode: 200, body: JSON.stringify(data) };
-};
