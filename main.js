5353f548594163a396c0065ca224cea59cc1e7f6
Zeph <agwproxies@gmail.com>
2025-06-30 05:38:50 -0400

    removed all hard coded shit

--- main.js
+++ main.js
@@ -377,20 +377,6 @@ function createProfileUploadButton(onProfileAdd) {
   };
 }
 
-const profiles = [
-  { img: 'https://cdn.discordapp.com/avatars/1126277279568367698/74d284282b825336027f80dab2a5ecf7.png?size=4096', name: 'Zeph', desc: 'i made ts so thats cool ig' },
-  { img: 'https://cdn.discordapp.com/avatars/366203114962812930/630edffda445f985fcf7d6da8a9c9604.png?size=4096', name: 'Remote', desc: 'remote is probably autistic' },
-  { img: 'https://images-ext-1.discordapp.net/external/iWUtqQNDZ8Y52lgEHRywV-IBQ4BNr6U484M9ZYsyNuM/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/1330709434044776493/85dcffe516eb74b5578bd25726e4ca74.png?format=webp&quality=lossless&width=239&height=239', name: 'Infuff', desc: 'Goated common chatter 💪' },
-  { img: 'https://images-ext-1.discordapp.net/external/eE_2BvedHApFOCF-SkpqfkL0YjQJFKAQzZfDJAogALI/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/1385733890353660028/37f1a301001f8f6ceb857da9b522145e.png?format=webp&quality=lossless&width=248&height=248', name: '#45 Player', desc: 'ban guts' },
-  { img: 'https://images-ext-1.discordapp.net/external/ywJC5PXtAAznJQiMj8T_QVM1bdTTlG4ttYXTnjvETeU/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/600579663781298176/c47f327688a7f7af77dd9185a8be74f4.png?format=webp&quality=lossless&width=810&height=810', name: 'Dr. Tempest', desc: '"the best jump stars cc"' },
-  { img: 'https://images-ext-1.discordapp.net/external/wJ0SVYcCdGpVbNEupSEvoRKxXpjXJYdCUDSE4KdRhCE/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/667912422341738496/bfd2a55d4b55cb01f40e68d5453f8359.png?format=webp&quality=lossless&width=282&height=282', name: 'Qich', desc: 'Everyone is good with me' },
-  { img: 'https://images-ext-1.discordapp.net/external/3o7nK356MmMqulb8uNbRG-yg20W_1Kcd7NtZhoAAcwU/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/397911610297024524/0e1eed142dfe8298d39014c7fe1ebbe6.png?format=webp&quality=lossless&width=282&height=282', name: 'FacuToilet', desc: 'buff guta' },
-  { img: 'https://cdn.discordapp.com/avatars/387335054206173184/272dd49f7692df610b668b13c47d1337.png?size=4096', name: 'Zoinkys', desc: 'N/A' },
-  { img: 'https://cdn.discordapp.com/avatars/594191661597327375/60f2145c98584eada12ff522e3800fea.png?size=4096', name: 'SenterSych', desc: '"im the goat"' },
-  { img: 'https://cdn.discordapp.com/avatars/1119296560468066404/b9e183255ddee74ed8e5dac8b2e146a3.png?size=4096', name: 'Justin', desc: 'N/A' },
-  {img: 'https://cdn.discordapp.com/guilds/1176637372721545316/users/529669497938903051/avatars/a_1b79bccfdc3d5629267bf6fb8094f171.gif?size=4096', name: 'DKR', desc: 'Guts is life' },
-];
-
 async function fetchProfiles() {
   const res = await fetch('/.netlify/functions/get-profiles');
   return await res.json();
@@ -416,4 +402,3 @@ createProfileUploadButton(async profile => {
 });
 
 createRepeatingTextBackground();
-createProfileGrid(profiles);
